afe9945cca12e9d2dcb6fbdf8082e408f567541b
MaGa <guser1996.gu@gmail.com>
2016-05-21 16:15:42 +0200

    cleaned up implementation of repository class. Transformed set optimization into sub class

--- src/physics/collision/CollisionRepository.java
+++ src/physics/collision/CollisionRepository.java
@@ -16,116 +16,11 @@ import java.util.HashMap;
 public class CollisionRepository
 {
 	/**
-	 * abstract class mapping entities to collider pairs. Used to customize
-	 * how entity queries should be handled.
-	 */
-	public interface EntityMapper
-	{
-		/**
-		 * @param e a given entity
-		 * @return list of collider pairs containing e
-		 * @throws IllegalArgumentException if e is not contained
-		 */
-		public ArrayList<ColliderPair> getFor (Entity e);
-
-		/**
-		 * @param e a given entity
-		 * @return true if entity exists in repository
-		 */
-		public boolean hasEntity (Entity e);
-
-		/**
-		 * adds c to the mapper's internals
-		 * @param c some collider pair
-		 */
-		public void addColliderPair (ColliderPair c);
-	}
-
-	/**
-	 * performs linear time searches instead of maintaining additional data structures
-	 */
-	public class NoMapper implements EntityMapper
-	{
-		@Override
-		public ArrayList<ColliderPair> getFor(Entity e)
-		{
-			ArrayList<ColliderPair> pairsForEntity = new ArrayList<>();
-			for (ColliderPair cPair : mColliderPairs)
-			{
-				if (cPair.hasEntity (e))
-					pairsForEntity.add (cPair);
-			}
-
-			return pairsForEntity;
-		}
-
-		@Override
-		public boolean hasEntity(Entity e)
-		{
-			return false;
-		}
-
-		@Override
-		public void addColliderPair (ColliderPair c) {}
-	}
-
-	/**
-	 * stores map to perform entity queries more quickly
-	 */
-	public class SetMapper implements EntityMapper
-	{
-
-		public SetMapper()
-		{
-			mEntityMap = new HashMap<>();
-		}
-
-		@Override
-		public ArrayList<ColliderPair> getFor(Entity e)
-		{
-			return mEntityMap.get (e);
-		}
-
-		@Override
-		public boolean hasEntity(Entity e)
-		{
-			return mEntityMap.containsKey (e);
-		}
-
-		@Override
-		public void addColliderPair (ColliderPair c)
-		{
-			addEntity (c.getFirst(), c);
-			addEntity (c.getSecond(), c);
-		}
-
-		/**
-		 * adds p to the list mapped to e or creates a new mapping if e is not yet contained
-		 * @param e
-		 * @param p
-		 */
-		private void addEntity (Entity e, ColliderPair p)
-		{
-			if (hasEntity (e))
-				mEntityMap.get (e).add (p);
-			else
-			{
-				ArrayList<ColliderPair> newColliderList = new ArrayList<>();
-				newColliderList.add (p);
-				mEntityMap.put (e, newColliderList);
-			}
-		}
-
-		private HashMap<Entity, ArrayList<ColliderPair>> mEntityMap;
-	}
-
-	/**
 	 * default constructor
 	 */
 	public CollisionRepository()
 	{
 		mColliderPairs = new ArrayList<>();
-		mEntityColliderMap = new HashMap<>();
 		mCache = new Cacher<>();
 	}
 
@@ -141,12 +36,14 @@ public class CollisionRepository
 	 */
 	public ArrayList<ColliderPair> getCollisionsFor (Entity e)
 	{
-		if (!mCache.isEmpty() && mCache.get().mEntity.equals (e))
-			return mCache.get().mPairs;
+		ArrayList<ColliderPair> collisions = new ArrayList<>();
 
-		ArrayList<ColliderPair> pairs = mEntityMapper.getFor (e);
-		mCache.forgetAndRemember (new EntityQuery (e, pairs));
-		return pairs;
+		for (ColliderPair cp : mColliderPairs)
+		{
+			if (cp.hasEntity (e))
+				collisions.add (cp);
+		}
+		return collisions;
 	}
 
 	/**
@@ -155,7 +52,13 @@ public class CollisionRepository
 	 */
 	public boolean hasEntity (Entity e)
 	{
-		return mEntityMapper.hasEntity (e);
+		for (ColliderPair cp : mColliderPairs)
+		{
+			if (cp.hasEntity (e))
+				return true;
+		}
+
+		return false;
 	}
 
 	/**
@@ -165,13 +68,12 @@ public class CollisionRepository
 	public void addColliderPair (ColliderPair p)
 	{
 		mColliderPairs.add (p);
-		mEntityMapper.addColliderPair (p);
 	}
 
 	/**
 	 * class storing entity query
 	 */
-	private class EntityQuery
+	protected class EntityQuery
 	{
 		public EntityQuery (Entity e, ArrayList<ColliderPair> cs)
 		{
@@ -183,10 +85,16 @@ public class CollisionRepository
 		public ArrayList<ColliderPair> mPairs;
 	}
 
+	/**rentityQueryCache
+	 * @return the cacher storing the last entity query
+	 */
+	protected Cacher<EntityQuery> getQueryCache()
+	{
+		return mCache;
+	}
+
 	/** stores list of colliding pairs */
 	private ArrayList<ColliderPair> mColliderPairs;
-	/** handles entity queries */
-	private EntityMapper mEntityMapper;
 	/** stores previous query */
 	private Cacher<EntityQuery> mCache;
 }
---
+++ src/physics/collision/SetCollisionRepository.java
@@ -0,0 +1,72 @@
+package physics.collision;
+
+import com.badlogic.ashley.core.Entity;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Extension to the collision repository to handle entity queries more efficiently.
+ * @autor martin
+ * created 21.05.2016
+ */
+public class SetCollisionRepository extends CollisionRepository
+{
+	/**
+	 * default constructor initializing fields
+	 */
+	public SetCollisionRepository()
+	{
+		mEntityMap = new HashMap<>();
+	}
+
+	/**
+	 * @param e a given entity
+	 * @return all collisions in which e is involved. Hands out the internal list storing them. Do not modify.
+	 */
+	public ArrayList<ColliderPair> getCollisionsFor (Entity e)
+	{
+		return mEntityMap.get (e);
+	}
+
+	/**
+	 * @param e a given entity
+	 * @return true if e is involved in a collision stored in this repository
+	 */
+	public boolean hasEntity (Entity e)
+	{
+		if (!getQueryCache().isEmpty() && getQueryCache().get().mEntity.equals (e))
+			return true;
+		return (mEntityMap.containsKey (e));
+	}
+
+	/**
+	 * adds cPair to the repository
+	 * @param cPair a given collider pair
+	 */
+	public void addColliderPair (ColliderPair cPair)
+	{
+		addEntity (cPair.getFirst());
+		addEntity (cPair.getSecond());
+	}
+
+
+	/**
+	 * adds p to the list mapped to e or creates a new mapping if e is not yet contained
+	 * @param e
+	 * @param p
+	 */
+	private void addEntity (Entity e, ColliderPair p)
+	{
+		if (hasEntity (e))
+			mEntityMap.get (e).add (p);
+		else
+		{
+			ArrayList<ColliderPair> newColliderList = new ArrayList<>();
+			newColliderList.add (p);
+			mEntityMap.put (e, newColliderList);
+		}
+	}
+
+	private HashMap<Entity, ArrayList<ColliderPair>> mEntityMap;
+}
