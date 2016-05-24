1739c821fc83edb05f337010f7f6d066d2dc357f
MaGa <guser1996.gu@gmail.com>
2016-05-24 09:35:24 +0200

    added sceletons for framework component factories

---
+++ src/framework/components/NextPlayerFactory.java
@@ -0,0 +1,10 @@
+package framework.components;
+
+import physics.components.ComponentFactory;
+
+/**
+ * Created by martin on 24.05.16.
+ */
+public class NextPlayerFactory extends ComponentFactory
+{
+}
