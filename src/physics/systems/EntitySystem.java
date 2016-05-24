12f438f9cf3634f4d9cf834e1481f5e9f4587ae2
MaGa <guser1996.gu@gmail.com>
2016-05-24 09:10:58 +0200

    changed entity listener: delegates call on changed emtity to system. Stores system. Changed compulsory interface of entity system: delegation methods add and remove entity.

--- src/physics/systems/EntitySystem.java
+++ src/physics/systems/EntitySystem.java
@@ -4,7 +4,7 @@ import com.badlogic.ashley.core.Entity;
 
 import java.util.HashSet;
 
-public class EntitySystem extends com.badlogic.ashley.core.EntitySystem 
+public abstract class EntitySystem extends com.badlogic.ashley.core.EntitySystem
 {
 	/**
 	 * @return a listener updating the physics.entities of this entity system
@@ -23,13 +23,26 @@ public class EntitySystem extends com.badlogic.ashley.core.EntitySystem
 		this.priority = priority;
 	}
 
-	/**
+    /**
+     * checks whether e belongs to the system and adds e if on successful check
+     * @param e a given entity
+     */
+    public abstract void addEntity (Entity e);
+
+    /**
+     * removes e from the system if e was in the system. otherwise nothing happens.
+     * @param e a given entity
+     */
+    public abstract void removeEntity (Entity e);
+
+    /**
 	 * @return iterable ImmutableArray of internal physics.entities
 	 */
 	protected HashSet<Entity> entities()
 	{
 		return mEntities;
 	}
+
 	
 	private HashSet<Entity> mEntities;
 }
