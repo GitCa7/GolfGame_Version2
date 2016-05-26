8456a13c4c38bded09017f7babba54dab44d7bfa
MaGa <guser1996.gu@gmail.com>
2016-05-26 11:50:11 +0200

    added comments to parameter classes, finished implementation, added normalization

--- src/physics/geometry/spatial/TetrahedronParameter.java
+++ src/physics/geometry/spatial/TetrahedronParameter.java
@@ -8,23 +8,65 @@ import java.util.HashSet;
 /**
  * Created by Alexander on 24.05.2016.
  */
-public class TetrahedronParameter {
-    public TetrahedronParameter(Vector3[] defining){
+public class TetrahedronParameter
+{
+
+    /**
+     *
+     * @param defining vectors to 4 vertices defining the tetrahedron
+     */
+    public TetrahedronParameter(Vector3[] defining)
+    {
         mDefining=defining;
+        norm();
+
+        mBuilder = new TetrahedronBuilder(defining);
 
     }
     public Tetrahedron instantiate() {
-        TetrahedronBuilder builder = new TetrahedronBuilder(mDefining);
-        return builder.build() ;
+        return mBuilder.build();
     }
 
     public Vector3[] getDefining(){return mDefining;}
 
     public boolean equals(TetrahedronParameter para) {
         HashSet<Vector3> set1= new HashSet<>(Arrays.asList(mDefining));
-        HashSet<Vector3> set2= new HashSet<>(Arrays.asList(para.mDefining));
-        return false;
+        return set1.containsAll(Arrays.asList(para.mDefining));
     }
 
+    /**
+     *
+     * @param vertices array of vertices
+     * @return the vertex in vertices the closest to the origin
+     */
+    private Vector3 findClostestToOrigin (Vector3[] vertices)
+    {
+        Vector3 closest = null;
+        float closestDistance = Float.MAX_VALUE;
+
+        for (Vector3 v : vertices)
+        {
+            float length = v.len();
+            if (closestDistance > length)
+            {
+                closest = v;
+                closestDistance = length;
+            }
+        }
+        return closest;
+    }
+
+    /**
+     * subtracts the closest vertex from all vertices in order to normalize the tetrahedron
+     * with respect to the origin.
+     */
+    private void norm()
+    {
+        Vector3 closest = findClostestToOrigin(mDefining);
+        for (Vector3 v : mDefining)
+            v.sub(closest);
+    }
+
+    private TetrahedronBuilder mBuilder;
     private Vector3[] mDefining;
 }
