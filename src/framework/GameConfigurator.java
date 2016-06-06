98ced8522420d32121da09011d21b950c86f876f
MaGa <guser1996.gu@gmail.com>
2016-06-06 22:01:40 +0200

    changed interface of game configurator to use more natural input in order to abstract away entities and components

--- src/framework/GameConfigurator.java
+++ src/framework/GameConfigurator.java
@@ -1,13 +1,23 @@
 package framework;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.gdx.math.Vector3;
+import framework.components.NameFactory;
+import framework.components.NextPlayerFactory;
+import framework.components.TurnFactory;
 import framework.entities.Player;
+import physics.components.*;
+import physics.constants.PhysicsCoefficients;
 import physics.entities.Ball;
 import framework.entities.EntityFactory;
 import physics.entities.Hole;
-import physics.systems.EntitySystem;
-import physics.systems.SystemsTracker;
+import physics.geometry.planar.Rectangle;
+import physics.geometry.planar.Shape;
+import physics.geometry.planar.Triangle;
+import physics.geometry.spatial.*;
+import physics.systems.*;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 /**
@@ -27,6 +37,15 @@ public class GameConfigurator
 		mEngine = new Engine();
 		mBallMap = new HashMap<>();
 		mHole = null;
+
+        mBallFactory = new EntityFactory(mSystemsTracker);
+        mPlayerFactory = new EntityFactory(mSystemsTracker);
+
+        mBallPositionFactory = new PositionFactory();
+        mBallMassFactory = new MassFactory();
+        mBallBodyFactory = new BodyFactory();
+
+        mPlayerNameFactory = new NameFactory();
 	}
 
 	/**
@@ -53,6 +72,59 @@ public class GameConfigurator
 		return new Game (mEngine, mBallMap, mHole);
 	}
 
+    /**
+     * Construct a new player and a new ball. Map this player to this ball. Players will take turns in the order
+     * they were added. The player added will contain all the necessary components for a turn-taking entitiy (see framework
+     * families) and the ball will contain all entities associated with the moving, accelerable, gravity-attracted, colliding and
+     * friction families (see physics families).
+     * @param name player's name
+     * @param ballRadius ball's radius
+     * @param ballMass ball's mass
+     * @param initBallPos ball's initial position
+     */
+    public void addPlayerAndBall(String name, float ballRadius, float ballMass, Vector3 initBallPos)
+    {
+        Ball b = constructBall(ballRadius, ballMass, initBallPos);
+        Player p = constructPlayer(name);
+        mBallMap.put(p, b);
+        mEngine.addEntity(b.mEntity);
+        mEngine.addEntity(p.mEntity);
+    }
+
+    /**
+     * Constructs a new obstacle. Every obstacle added this way will contain all the entities associated with the colliding
+     * family (see physics families).
+     * @param mass the mass of the obstacle
+     * @param bodySolids a collection of solids. The vertex with the smallest euclidean distance to the origin is fixed as the
+     *                   position of the obstacle.
+     */
+    public void addObstacle(float mass, Collection<Solid> bodySolids)
+    {
+        //@TODO implement extrinsic state of flyweight solids. Check this after implementing.
+        throw new UnsupportedOperationException("obstacles are not yet supported and thus not implemented");
+    }
+
+    /**
+     * Constructs a new hole. This hole has all components associtated with the NAME_THEM_HERE! families.
+     * @param holeShape The shape of the hole, embedding the position where the hole is located.
+     */
+    public void setHole(Shape holeShape)
+    {
+        //@TODO implement hole
+        throw new UnsupportedOperationException("the hole is not yet relevant and thus not implemented");
+    }
+
+    /**
+     * Constructs a new obstacle for the terrain within the given area. This will transform the given mesh triangles
+     * into tetrahedra used for collision checking.
+     * @param meshTriangles a collection of triangles approximating the surface of the terrain.
+     */
+    public void setTerrain(Collection<Triangle> meshTriangles)
+    {
+        //@TODO implement terrain setting
+        throw new UnsupportedOperationException("the terrain is not yet relevant and thus not implemented");
+    }
+
 	/**
 	 * adds a new ball b for player p.
 	 * Precondition: there are no balls associated with p yet.
@@ -66,25 +138,78 @@ public class GameConfigurator
 		mBallMap.put (p, b);
 	}
 
-	/**
-	 * sets the hole stored to h
-	 * @param h a hole object
-	 */
-	public void setHole (Hole h)
-	{
-		mHole = h;
-	}
+    /**
+     * constructs ball of given radius, mass and sets the initial position of it
+     * SIMPLIFICATION: A BOX IS USED TO APPROXIMATE THE BALL
+     * @param radius ball's radius
+     * @param mass ball's mass
+     * @param initPos ball's initial position
+     * @return a ball instance containing a ball entity with components for the desired properties
+     */
+    private Ball constructBall(float radius, float mass, Vector3 initPos)
+    {
+        //@TODO tetrahedrize sphere
+        mBallPositionFactory.setVector(initPos);
+        mBallMassFactory.setParameter(mass);
+        mBallBodyFactory.clear();
 
-	/**
-	 * Produces count physics.entities using e and adds them to the engine.
-	 * @param e a factory object producing physics.entities
-	 * @param count the number of physics.entities to produce in e
-	 */
-	public void addEntities (EntityFactory e, int count)
-	{
-		for (int cAdd = 0; cAdd < count; ++cAdd)
-			mEngine.addEntity (e.produce());
-	}
+        Vector3 sphereDep = new Vector3 (2*radius, 0, 0);
+        Vector3 sphereWid = new Vector3 (0, 2*radius, 0);
+        Vector3 sphereHig = new Vector3 (0, 0, 2*radius);
+        BoxParameter sphereBound = new BoxParameter(sphereDep, sphereWid, sphereHig);
+        Box sphereBoundingBox = BoxPool.getInstance().getInstance(sphereBound);
+        mBallBodyFactory.addSolid(sphereBoundingBox);
+        return new Ball (mBallFactory.produce());
+    }
+
+    /**
+     * constructs player. The next player component still needs to be set properly, as it contains null by default.
+     * @param name player's name
+     * @return a new player wrapper containing a entity having components for the desired properties
+     */
+    private Player constructPlayer(String name)
+    {
+        mPlayerNameFactory.set(name);
+        return new Player (mPlayerFactory.produce());
+    }
+
+    /**
+     * constructs component bundles neccesary for the entity factories to work
+     */
+    private void initFactories()
+    {
+        //additional component factories for balls
+        VelocityFactory ballVelocityFactory = new VelocityFactory();
+        ForceFactory ballForceFactory = new ForceFactory();
+        GravityForceFactory ballGravityFactory = new GravityForceFactory();
+        FrictionFactory ballFrictionFactory = new FrictionFactory();
+        //set default parameters of component factories for balls we dont need to change
+        ballVelocityFactory.setVector(new Vector3());
+        ballForceFactory.setVector(new Vector3());
+        ballGravityFactory.setParameter(new Vector3());
+        ballFrictionFactory.setParameter(PhysicsCoefficients.STATIC_FRICTION, PhysicsCoefficients.DYNAMIC_FRICTION, 0, 0);
+        //construct ball component bundles
+        ComponentBundle ballPosition = new ComponentBundle(mBallPositionFactory);
+        ComponentBundle ballVelocity = new ComponentBundle(ballVelocityFactory, new MovementFactory());
+        ComponentBundle ballForce = new ComponentBundle(ballForceFactory, new ForceApplyFactory());
+        ComponentBundle ballFriction = new ComponentBundle(ballFrictionFactory, new FrictionSystemFactory());
+        ComponentBundle ballMass = new ComponentBundle(mBallMassFactory);
+        ComponentBundle ballBody = new ComponentBundle(mBallBodyFactory);
+        //add bundles to ball factory
+        mBallFactory.addComponent(ballPosition, ballVelocity, ballForce, ballFriction, ballMass, ballBody);
+
+        //additional component factories for players
+        TurnFactory playerTurnFactory = new TurnFactory();
+        NextPlayerFactory playerNextFactory = new NextPlayerFactory();
+        //set default parameter of component facotires for players we dont need to change
+        playerNextFactory.setNextPlayer(null);
+        //construct player component bundles
+        ComponentBundle playerTurn = new ComponentBundle(playerTurnFactory);
+        ComponentBundle playerNext = new ComponentBundle(playerNextFactory);
+        ComponentBundle playerName = new ComponentBundle(mPlayerNameFactory);
+        //add bunldes to player factory
+        mPlayerFactory.addComponent(playerTurn, playerNext, playerName);
+    }
 
 	/**
 	 * adds all systems tracked by the systems tracker to the engine
@@ -103,4 +228,11 @@ public class GameConfigurator
 	private Engine mEngine;
 	/** objects tracking physics.systems in use */
 	private SystemsTracker mSystemsTracker;
+    /** entity factories used to instantiate certain kinds of entities */
+    private EntityFactory mBallFactory, mPlayerFactory;
+    /** handles for component factories used in entity factories */
+    private PositionFactory mBallPositionFactory;
+    private MassFactory mBallMassFactory;
+    private BodyFactory mBallBodyFactory;
+    private NameFactory mPlayerNameFactory;
 }
--- src/framework/components/TurnFactory.java
+++ src/framework/components/TurnFactory.java
@@ -4,12 +4,14 @@ import com.badlogic.ashley.core.Component;
 import physics.components.ComponentFactory;
 
 /**
- * Created by martin on 24.05.16.
+ * class producing turn components
+ * @author martin
  */
 public class TurnFactory implements ComponentFactory {
 
     @Override
-    public Component produce() {
-        return null;
+    public Component produce()
+    {
+        return new Turn();
     }
 }
