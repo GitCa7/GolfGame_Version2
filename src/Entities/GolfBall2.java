3c2bca895ae978c76d78b4e4f6ac5c5f0b64e00b
RobinSims <robin@silberschmuck.com>
2016-06-09 14:50:20 +0200

    Stack trace error fixed

---
+++ src/Entities/GolfBall2.java
@@ -0,0 +1,27 @@
+package Entities;
+
+import com.badlogic.ashley.core.Entity;
+
+import physics.entities.Ball;
+
+public class GolfBall2 extends Entity{
+	//public Entity mEntity;
+	public 
+	
+	public GolfBall2()	{
+		
+	}
+	
+	public boolean equals(Ball comp)
+	{
+		return (comp.mEntity == this.mEntity);
+	}
+	
+	
+	public static void main(String[] args)	{
+		Entity mat = new Entity();
+		Position pos = new Component();
+		mat.add()
+	}
+
+}
