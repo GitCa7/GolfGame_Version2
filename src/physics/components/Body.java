1892e0c88d88870686d9d3bef5834b0bb0c09170
MarcelSchm11 <macschm11@gmail.com>
2016-06-21 16:35:39 +0200

    implemented bounding box in body

--- src/physics/components/Body.java
+++ src/physics/components/Body.java
@@ -2,9 +2,11 @@ package physics.components;
 
 
 import com.badlogic.gdx.math.Vector3;
+import physics.geometry.spatial.BoundingBox;
 import physics.geometry.spatial.Solid;
 import physics.geometry.spatial.SolidTranslator;
 
+import java.util.Collection;
 import java.util.HashSet;
 
 /**
@@ -13,6 +15,19 @@ import java.util.HashSet;
  */
 public class Body extends HashSet<SolidTranslator> implements Component
 {
+
+
+	public Body()
+	{
+		mBound = null;
+	}
+
+
+	public Body(Collection<SolidTranslator> solids)
+	{
+		super (solids);
+		mBound = new BoundingBox(solids);
+	}
 
 	/**
 	 * @return a new body containing deep-copied solid translators
@@ -25,10 +40,34 @@ public class Body extends HashSet<SolidTranslator> implements Component
 		return newBody;
 	}
 
+	public BoundingBox getBound()
+	{
+		return mBound;
+	}
+
+	public boolean add(SolidTranslator add)
+	{
+		boolean success = super.add (add);
+		if (success)
+			mBound = new BoundingBox(this);
+		return success;
+	}
+
+	public boolean remove(SolidTranslator remove)
+	{
+		boolean success = super.remove(remove);
+		if (success)
+			mBound = new BoundingBox(this);
+		return success;
+	}
+
+
 	public void setPosition(Vector3 newPosition)
 	{
 		for (SolidTranslator s : this)
 			s.setPosition(newPosition);
 	}
 
+
+	private BoundingBox mBound;
 }
