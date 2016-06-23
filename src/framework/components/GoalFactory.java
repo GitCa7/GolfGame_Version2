bbcff91c6f5412a99218700cdb25e8ddba7b3b63
MaGa <guser1996.gu@gmail.com>
2016-06-23 20:20:19 +0200

    fixed wrong imports

--- src/framework/components/GoalFactory.java
+++ src/framework/components/GoalFactory.java
@@ -1,10 +1,8 @@
 package framework.components;
 
-import com.badlogic.ashley.core.Component;
 import com.badlogic.gdx.math.Vector3;
 import physics.geometry.spatial.Solid;
 import physics.geometry.spatial.SolidTranslator;
-import sun.awt.ComponentFactory;
 
 /**
  * Class creating goal instances
